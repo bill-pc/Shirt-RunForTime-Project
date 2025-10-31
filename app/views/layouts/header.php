6356f9ef3ff55b3e0ec1b1493ff955c13b856e1e
phuongduy1302 <87838011+phuongduy1302@users.noreply.github.com>
2025-10-31 23:19:04 +0700

    Add files via upload

---
+++ app/views/layouts/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hệ thống Quản lý Sản Xuất</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <link rel="stylesheet" href="public/css/style.css">
+</head>
+<body>
+<header class="header">
+    <div class="logo" id="logo-interlock">
+        <span>R</span><span class="middle-letter">F</span><span>T</span>
+    </div>
+    <h1>HỆ THỐNG QUẢN LÝ SẢN XUẤT</h1>
+    <div class="user-controls">
+        <i class="fa-solid fa-magnifying-glass"></i>
+        <i class="fa-regular fa-bell"></i>
+        <a href="#" class="user-link">
+            <i class="fa-regular fa-circle-user"></i>
+            <span>Tên nhân viên</span>
+        </a>
+    </div>
+</header>
