89e586dbf121b7cc380c480ff308d649b58eafdf
phuongduy1302 <87838011+phuongduy1302@users.noreply.github.com>
2025-12-03 18:52:39 +0700

    Delete Shirt-RunForTime-Project-main directory

--- Shirt-RunForTime-Project-main/public/js/congnhan.js
+++
@@ -1,9 +0,0 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     const sidebarList = document.getElementById("sidebarList");
-//     if (!sidebarList) return;
-
-//     sidebarList.innerHTML = `
-//         <li><a href="#" class="sidebar-link">Lịch làm việc</a></li>
-//         <li><a href="#" class="sidebar-link">Báo cáo sự cố</a></li>
-//     `;
-// });
